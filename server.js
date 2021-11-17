234a74f85e8429e6b9da30b54ecf256d34b15669
jpumki <jpumki96@gmail.com>
2021-11-16 19:23:03 -0500

    update

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+require('./services/movies-service')(app);
+
+app.listen(4000);
